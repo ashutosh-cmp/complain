e394e58e9b8706fb2ce5b8f5f10b2ba1f60e4ca4
Aniket Maurya <aniketmaurya.1601@gmail.com>
2024-07-31 23:36:18 +0530

    remove waste file

--- resources/views/dashboard/shared/footer.blade.php
+++ resources/views/dashboard/shared/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer">
-    <div><a href="https://coreui.io">CoreUI</a> &copy; 2020 creativeLabs.</div>
-    <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
+    <div><a href="#">CODE GENIE</a> &copy; 2024.</div>
+    <div class="ml-auto">Powered by&nbsp;<a href="#">CodeGenie.com</a></div>
 </footer>
